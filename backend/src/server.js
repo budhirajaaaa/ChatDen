9ef7f17f6cb22b6e285c797000f2a61ae7828687
budhirajaaaa <dakshbudhiraja2@gmail.com>
2026-01-16 00:58:49 +0530

    deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,9 @@ import express from "express"
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
-
+import path from "path"
 dotenv.config({ path: "./src/.env" })
+
 const app = express()
 app.use((req, res, next) => {
   console.log("Incoming:", req.method, req.url)
@@ -11,6 +12,15 @@ app.use((req, res, next) => {
 })
 app.use('/api/auth',authRoutes)
 app.use('/api/message',messageRoute)
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static("../frontend/build"))
+
+// For all routes not starting with /api
+  app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.resolve("../frontend/build/index.html"))
+  })
+}
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Example app listening on port number ${port}`)
